91ab31fbf389bc5353794a081812de3a38a0fbc3
phanbadu <duphan03@gmail.com>
2024-11-23 06:47:05 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div className="min-h-screen w-full custom flex justify-center items-center text-primary">
+            <h1 className="text-7xl w-full h-full font-bold uppercase flex flex-col items-center justify-center gap-10 ">
+                Welcome to the
+                <span className="header__hero--heading-gradient">e-commerce</span>
+                project. 👌
+            </h1>
+        </div>
+    );
+}
